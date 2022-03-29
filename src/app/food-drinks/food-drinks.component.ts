6af336c9c77f0ee3e8313f1f28b28b907a0aef48
Joao Vieira <joaoantoniopintovieira13@gmail.com>
2022-03-29 17:41:14 +0100

    Products Grid

--- src/app/food-drinks/food-drinks.component.ts
+++ src/app/food-drinks/food-drinks.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FoodDrinksComponent implements OnInit {
 
+  public products = [
+    { id: 1, name: 'Coca Cola', price: '1.00', image: 'https://previews.123rf.com/images/rvlsoft/rvlsoft1509/rvlsoft150900014/45570624-big-hamburger-su-sfondo-bianco.jpg' },
+    { id: 2, name: 'Pepsi', price: '1.00', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqGT9OxdI7j78oYbAWgltb9O8Ek_0XjRaPsg&usqp=CAU' },
+    { id: 3, name: 'Sprite', price: '1.00', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwQRczcsW1q6aRz39DhDJx0FmJgbYa3OM85A&usqp=CAU' },
+    { id: 4, name: 'Fanta', price: '1.00', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2dI66fU-yV3E7VcHivZ9EnRUFb-xYUnnPkA&usqp=CAU' },
+    { id: 1, name: 'Coca Cola', price: '1.00', image: 'https://previews.123rf.com/images/rvlsoft/rvlsoft1509/rvlsoft150900014/45570624-big-hamburger-su-sfondo-bianco.jpg' },
+    { id: 2, name: 'Pepsi', price: '1.00', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqGT9OxdI7j78oYbAWgltb9O8Ek_0XjRaPsg&usqp=CAU' },
+    { id: 3, name: 'Sprite', price: '1.00', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwQRczcsW1q6aRz39DhDJx0FmJgbYa3OM85A&usqp=CAU' },
+    { id: 4, name: 'Fanta', price: '1.00', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2dI66fU-yV3E7VcHivZ9EnRUFb-xYUnnPkA&usqp=CAU' },
+    { id: 1, name: 'Coca Cola', price: '1.00', image: 'https://previews.123rf.com/images/rvlsoft/rvlsoft1509/rvlsoft150900014/45570624-big-hamburger-su-sfondo-bianco.jpg' },
+    { id: 2, name: 'Pepsi', price: '1.00', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqGT9OxdI7j78oYbAWgltb9O8Ek_0XjRaPsg&usqp=CAU' },
+    { id: 3, name: 'Sprite', price: '1.00', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwQRczcsW1q6aRz39DhDJx0FmJgbYa3OM85A&usqp=CAU' },
+    { id: 4, name: 'Fanta', price: '1.00', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2dI66fU-yV3E7VcHivZ9EnRUFb-xYUnnPkA&usqp=CAU' },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
